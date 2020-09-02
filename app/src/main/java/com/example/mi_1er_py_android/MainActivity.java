b3bfb3a215d278b341899569ca930cb0c292de5e
carlosarielburitica <arielburitica@gmail.com>
2020-09-01 20:57:41 -0500

    Proyecto Android Cousera

---
+++ app/src/main/java/com/example/mi_1er_py_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mi_1er_py_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
